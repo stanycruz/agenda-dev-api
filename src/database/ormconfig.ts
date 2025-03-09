89d9045ed76ec597dfe2cc5572daa2e0fca92f80
Stanislaw Cruz <stanycruz@outlook.com>
2025-03-09 17:12:33 -0300

    feat: add configuration for typeorm with migrations

---
+++ src/database/ormconfig.ts
@@ -0,0 +1,14 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+export const ormconfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: +(process.env.DB_PORT || 5432),
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || 'postgres',
+  database: process.env.DB_NAME || 'agenda_dev',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  synchronize: false,
+  logging: true,
+};
