c8714461ae2bc797d1edc3e9f74930e96a6b935b
Stanislaw Cruz <stanycruz@outlook.com>
2025-03-13 11:24:42 -0300

    feat: add module peerson

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { PersonModule } from './modules/person/person.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PersonModule],
   controllers: [AppController],
   providers: [AppService],
 })
